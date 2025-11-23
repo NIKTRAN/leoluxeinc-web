32dbe232aa5236299c46d547417fa1736339a804
NIKTRAN <nicktrantrituan@gmail.com>
2025-11-23 02:11:07 -0500

    some basic taskbar from some plug-ins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+import { SiteNavbar } from "@/components/ui/site-navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,11 +14,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Leo Lux ",
+  title: "LEO LUXE INC",
   description: "Deals on Luxury items",
-    icons: {
-    icon: "public/images/brand assets/Leo Luxe Logo.svg",
-  },
+  
 };
 
 export default function RootLayout({
@@ -31,15 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div>
-          {" "}
-          <Link href={"/."}> Home </Link>
-          <Link href={"/Product"}> Product </Link>
-          <Link href={"/Contact"}> Contact </Link>
-          <Link href={"/About"}> About Us </Link>
-        
-        </div>
-        
+        <SiteNavbar />
         {children}
       </body>
     </html>
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  devIndicators: false,
+  // devIndicators: false,
 };
 
 export default nextConfig;
