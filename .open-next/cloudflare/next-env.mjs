9208ce63e38883a8ed341a1b51b955aeae0e7282
NIKTRAN <nicktrantrituan@gmail.com>
2026-01-02 16:12:23 -0500

    idk

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"DATABASE_URL":"postgresql://neondb_owner:npg_nrBaRoec81Kh@ep-bitter-rain-ahuysax3.c-3.us-east-1.aws.neon.tech/leoluxeinc_web?sslmode=require&channel_binding=require"};
+export const development = {"DATABASE_URL":"postgresql://neondb_owner:npg_nrBaRoec81Kh@ep-bitter-rain-ahuysax3.c-3.us-east-1.aws.neon.tech/leoluxeinc_web?sslmode=require&channel_binding=require"};
+export const test = {"DATABASE_URL":"postgresql://neondb_owner:npg_nrBaRoec81Kh@ep-bitter-rain-ahuysax3.c-3.us-east-1.aws.neon.tech/leoluxeinc_web?sslmode=require&channel_binding=require"};
